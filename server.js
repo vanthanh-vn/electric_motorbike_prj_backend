0fe876be37232ef27a08b2bd747a06bd7cd1daae
Nguyen Van Thanh <thanhnguyenvan762@gmail.com>
2026-03-21 13:35:07 +0700

    them cors

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import motorbikeRoutes from "./routes/motorbike.route.js";
 import categoryRoutes from "./routes/category.route.js";
 import carRouter from "./routes/car.route.js";
@@ -8,6 +9,7 @@ dotenv.config();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/motorbikes", motorbikeRoutes);
